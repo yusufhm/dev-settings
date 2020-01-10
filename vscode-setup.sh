742a34406b869789708f82a2a1db6c9cb1f8de88
Yusuf Hasan Miyan <yusufh@protonmail.com>
2020-01-10 16:50:25 +1100

    Added vscode settings.

--- vscode-setup.sh
+++ vscode-setup.sh
@@ -10,7 +10,8 @@ vs_user_settings_file=${vs_user_dir}/settings.json
 if [ -e "${vs_user_settings_file}" ]; then
     echo "VSCode installed and user settings file exists."
 elif [ -d "${vs_user_dir}" ]; then
-    echo "VSCode installed but no user settings file yet."
+    echo "VSCode installed but no user settings file yet; linking file."
+    ln -s `pwd`/vscode-settings.json ~/Library/Application\ Support/Code/User/settings.json
 else
     echo "VSCode not installed"
 fi
