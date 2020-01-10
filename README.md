1875cf352fbb72e119fe07beb54f9295c1263ac7
Yusuf Hasan Miyan <yusufh@protonmail.com>
2020-01-10 16:38:22 +1100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+A repository to capture my must-have Mac configs.
+
+## Requirements
+  - [homebrew](https://brew.sh/)
+  - [ahoy](https://github.com/ahoy-cli/ahoy)
---
+++ vscode-setup.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Strict bash mode.
+set -o errtrace
+set -o errexit
+set -o nounset
+
+vs_user_dir=~/Library/Application\ Support/Code/User
+vs_user_settings_file=${vs_user_dir}/settings.json
+if [ -e "${vs_user_settings_file}" ]; then
+    echo "VSCode installed and user settings file exists."
+elif [ -d "${vs_user_dir}" ]; then
+    echo "VSCode installed but no user settings file yet."
+else
+    echo "VSCode not installed"
+fi
