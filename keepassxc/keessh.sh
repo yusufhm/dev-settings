c200054cb5b442dc9d0e5de2a4d46c1a6c4d3bff
Yusuf A. Hasan Miyan <yusufh@protonmail.com>
2023-08-16 16:27:30 +1000

    added keessh script

---
+++ keepassxc/keessh.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# This script is used to add an SSH key from KeePassXC to the ssh-agent.
+set -eu
+
+if [ $# -eq 0 ]; then
+  echo "Usage: $0 [entry] [attachment]"
+  echo Possible entries:
+  keepassxc-cli search ~/keepass/Passwords.kdbx '*attachment:"kee"'
+  exit 0
+elif [ $# -eq 1 ]; then
+  echo "Usage: $0 $1 [attachment]"
+  echo Possible attachments:
+  keepassxc-cli show --show-attachments --attributes Title ~/keepass/Passwords.kdbx $1
+  exit 0
+fi
+
+keepassxc-cli attachment-export ~/keepass/Passwords.kdbx $1 $2 --stdout | ssh-add -
